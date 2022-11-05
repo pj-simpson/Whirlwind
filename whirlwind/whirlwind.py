432cf86dc297ef183d168c9c76a4463c9e6cfd4d
peter simpson <petersimpson@peters-Air.cust.communityfibre.co.uk>
2022-11-05 01:40:51 +0000

    dynamic routing + config

--- whirlwind/whirlwind.py
+++ whirlwind/whirlwind.py
@@ -2,69 +2,60 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 import random
+import yaml
 from tornado.httpclient import AsyncHTTPClient
 
 from tornado.options import define, options
 
 define("port", default=8000, type=int)
 
-class HostRouter(tornado.web.RequestHandler):
 
-    A_BACKENDS = ['localhost:8081', 'localhost:8082']
-    B_BACKENDS = ['localhost:9081', 'localhost:9082']
+def load_configuration(path):
+    with open(path) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+    return config
+
+
+config = load_configuration('loadbalancer.yaml')
+
+
+class HostRouter(tornado.web.RequestHandler):
 
     async def get(self):
         http_client = AsyncHTTPClient()
         host_header = self.request.headers['Host']
+        for entry in config['hosts']:
+            if host_header == entry['host']:
+                response = await http_client.fetch(f'http://{random.choice(entry["servers"])}')
+                http_client.close()
+                response_message = response.body.decode("utf-8")
+                self.set_status(response.code)
+                self.write(response_message)
+            else:
+                self.set_status(404, 'Not Found')
 
-        if host_header == 'www.a.com':
-            response = await http_client.fetch(f'http://{random.choice(self.A_BACKENDS)}')
-            http_client.close()
-            response_message = response.body.decode("utf-8")
-            self.set_status(response.code)
-            self.write(response_message)
-        elif host_header == 'www.b.com':
-            response = await http_client.fetch(f'http://{random.choice(self.B_BACKENDS)}')
-            http_client.close()
-            response_message = response.body.decode("utf-8")
-            self.set_status(response.code)
-            self.write(response_message)
-        else:
-            self.set_status(404,'Not Found')
+class PathRouter(tornado.web.RequestHandler):
 
-class APathRouter(tornado.web.RequestHandler):
-    A_BACKENDS = ['localhost:8081', 'localhost:8082']
-
-    async def get(self):
+    async def get(self,path):
         http_client = AsyncHTTPClient()
+        for entry in config['paths']:
+            if ('/' + path) == entry['path']:
+                response = await http_client.fetch(f'http://{random.choice(entry["servers"])}')
+                http_client.close()
+                response_message = response.body.decode("utf-8")
+                self.set_status(response.code)
+                self.write(response_message)
+            else:
+                self.set_status(404, 'Not Found')
 
-        response = await http_client.fetch(f'http://{random.choice(self.A_BACKENDS)}')
-        http_client.close()
-        response_message = response.body.decode("utf-8")
-        self.set_status(response.code)
-        self.write(response_message)
 
-class BPathRouter(tornado.web.RequestHandler):
-
-    B_BACKENDS = ['localhost:9081', 'localhost:9082']
-
-
-    async def get(self):
-        http_client = AsyncHTTPClient()
-
-        response = await http_client.fetch(f'http://{random.choice(self.B_BACKENDS)}')
-        http_client.close()
-        response_message = response.body.decode("utf-8")
-        self.set_status(response.code)
-        self.write(response_message)
 
 
 def make_app():
     return tornado.web.Application(
         handlers=[
             (r"/", HostRouter),
-            (r"/a", APathRouter),
-            (r"/b", BPathRouter),
+            (r"/(\w+)", PathRouter),
         ]
     )
 
